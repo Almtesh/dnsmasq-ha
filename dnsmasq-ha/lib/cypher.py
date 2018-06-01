f02130ab46cef03da0e08dbf5de4a613c3471ee5
Gilles MOREL <contact@gilles-morel.fr>
2018-06-01 13:44:11 +0200

    Code factorisation on cypher lib

--- dnsmasq-ha/lib/cypher.py
+++ dnsmasq-ha/lib/cypher.py
@@ -3,21 +3,21 @@ Inspired from https://gist.github.com/syedrakib/d71c463fc61852b8d366
 Library to simply encrypt exchanged thank to shared secret
 '''
 
-from Crypto.Cipher import AES
+from Crypto.Cipher.AES import new as AES
 from hashlib import sha256
 
 padding_character = bytes ('\0', 'UTF-8')
 
 
-def generate_aes_key_from_secret (secret):
+def generate_aes_from_secret (secret):
 	'''
-	This function generates a secret for AES based on secret
+	This function generates a AES object from the secret
 	secret must be bytes
 	'''
 	if type (secret) != bytes:
 		raise (TypeError ('secret must be bytes'))
 	secret_key = sha256 (secret).digest ()
-	return (secret_key)
+	return (AES (secret_key))
 
 
 def encrypt (message, secret):
@@ -27,8 +27,9 @@ def encrypt (message, secret):
 	'''
 	if type (message) != bytes:
 		raise (TypeError ('message must be bytes'))
-	cipher = AES.new (generate_aes_key_from_secret (secret))
-	padded = message + (padding_character * ((16-len (message)) % 16))
+	cipher = generate_aes_from_secret (secret)
+	# Message must be padded to have a 16-divisible size (required by AES)
+	padded = message + (padding_character * ((16 - len (message)) % 16))
 	encrypted = cipher.encrypt (padded)
 	return (encrypted)
 
@@ -40,8 +41,9 @@ def decrypt (message, secret):
 	'''
 	if type (message) != bytes:
 		raise (TypeError ('message must be bytes'))
-	cipher = AES.new (generate_aes_key_from_secret (secret))
+	cipher = generate_aes_from_secret (secret)
 	decrypted = cipher.decrypt (encrypted_msg)
+	# Remove padding from decrypted message
 	unpadded = decrypted.rstrip (padding_character)
 	return (unpadded)
 
